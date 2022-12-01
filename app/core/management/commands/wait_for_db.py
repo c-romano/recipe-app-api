e820f98099052e0fe26b217be7b29aee5ebb7d60
c-romano <romano.conor@gmail.com>
2022-11-30 20:34:29 -0500

    8.39 db delay test bug fixes

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -7,5 +7,5 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     """Django command to wait for database."""
 
-    def handle(delf, *args, **options):
+    def handle(self, *args, **options):
         pass
