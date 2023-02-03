319ae40403706ff596058f623ba4f8fe6c8529af
c-romano <romano.conor@gmail.com>
2023-02-02 20:37:12 -0500

    17.134 added queryset base recipe view changes

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -102,8 +102,8 @@ class PrivateIngredientsApiTests(TestCase):
 
     def test_filter_ingredients_assigned_to_recipes(self):
         """Test listing ingredients by those assigned to recipes."""
-        in1 = Ingredient.objects.create(self=self.user, name='Apples')
-        in2 = Ingredient.objects.create(self=self.user, name='Turkey')
+        in1 = Ingredient.objects.create(user=self.user, name='Apples')
+        in2 = Ingredient.objects.create(user=self.user, name='Turkey')
         recipe = Recipe.objects.create(
             title='Apple Crumble',
             time_minutes=5,
